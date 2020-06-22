84937c00f64201cdc798bdad8c87431b0d90cdef
FarhadNawab <farhadnawab.official@gmail.com>
2020-06-22 23:54:14 +0500

    Hamburger functionality added on tablet/mobile & smooth scrolling to sections

--- js/main.js
+++ js/main.js
@@ -10,4 +10,30 @@ $(document).ready(function(){
             }
         }
     })
+
+    /*** Smooth Scroll ***/
+    $(document).on('click', 'a[href^="#"]', function(e) {
+        // target element id
+        var id = $(this).attr('href');
+
+        // target element
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+
+        // top position relative to the document
+        var pos = $id.offset().top;
+
+        // animated top scrolling
+        $('body, html').animate({scrollTop: (pos)});
+    });
+
+    /** Nav Toggle **/
+    $(document).on("click", "#nav-toggle", function(){
+        $(".mobile-menu").slideToggle().toggleClass("active");
+    });
 });
